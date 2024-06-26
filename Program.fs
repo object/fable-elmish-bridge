69431a9dcc48da937c4670d8777cdb49707b9730
Vagif Abilov <vagif.abilov@nrk.no>
2024-06-26 07:40:18 +0200

    Created Fable project with Fable.Elmish and Bridge.Elmish.

---
+++ Program.fs
@@ -0,0 +1,34 @@
+﻿module App
+
+open Fable.Core
+open Fable.React
+
+open Elmish
+open Elmish.React
+
+open Elmish
+open Elmish.Bridge
+open Fable.React
+
+type Model = { Count: int }
+
+type Messages = Incr | Decr 
+
+let init () = { Count = 0 }, Cmd.none
+
+let update msg model = 
+    match msg with
+    | Incr -> { Count = model.Count + 1 }
+    | Decr -> { Count = model.Count - 1 }
+    , Cmd.none
+
+let view model dispatch = 
+    div 
+      []
+      [
+      ]
+
+Program.mkProgram init update view
+|> Program.withBridge "localhost"
+|> Program.withReactSynchronous "elmish-app"
+|> Program.run
